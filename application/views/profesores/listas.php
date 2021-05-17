435358c976f954351b18ebc8e150de2f31dd6889
Rams <ramiroaltamirano.lpz@gmail.com>
2021-05-17 00:22:20 -0500

    listas

--- application/views/profesores/listas.php
+++ application/views/profesores/listas.php
@@ -2,7 +2,7 @@
 
     <div class="row">
         <?php foreach ($carreras as $key => $value) :?>
-        <div class="col-lg-3 col-md-4 col-sm-6 mb-3 p-2">
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-3 p-git 2">
             <a href="<?=base_url('profesores/carrera/'.str_replace(" ",'_',$value))?>" class="list-group-item bg-white h-100">
                 <h5><?=$value?></h5>
             </a>
